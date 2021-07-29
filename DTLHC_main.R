b86d5ce04b037440e62949ec2b8ed51986123135
rachidabbas-hub <64604146+rachidabbas-hub@users.noreply.github.com>
2021-07-30 00:41:35 +0200

    fix issues

--- DTLHC_main.R
+++ DTLHC_main.R
@@ -40,7 +40,12 @@ s6	=	c( hazard.medium,	hazard.small,	 hazard.null) #6: 2 better than HC (small/m
 s7	=	c( hazard.medium,	hazard.medium, hazard.null) #7: 2 better than HC medium effects
 s8	=	c( hazard.large,	hazard.medium, hazard.null) #8: 2 better than HC (medium/large) effects
 s9	=	c( hazard.large,	hazard.large,  hazard.null) #9: 2 better than HC large effects
+scenarios <- list(s1,s2,s3,s4,s5,s6,s7,s8,s9)
+
 
 # Reproduce results under scenario 3 for the drop-thelosers design with 30 patients per arm at first stage and 30 patients at second stage
 sim <- droptheloser(seed=2306,hazard.exp=s3, n1.perarm=30,n2=30, requiredfwer=0.1, K=3,hazard.null=hazard.null, shape=0.9509, hazard.censoring = 0.5, followuptime=2, recruitmentrate= 50)
 sim$disjunctive.power
+sim$theoric.d.power
+
+
